f96c2b10acc87640bd7a92dd2fde68ad15e41e47
ODude Navneet Gupta <navneet@odude.com>
2020-05-02 20:15:05 +0545

    1.0.20

--- includes/class-flexi_ajax_delete.php
+++ includes/class-flexi_ajax_delete.php
@@ -35,7 +35,7 @@ class flexi_delete_post
   }
 
   if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-   $result = evalue_encode($result);
+   $result = json_encode($result);
    echo $result;
   } else {
    header("Location: " . $_SERVER["HTTP_REFERER"]);
