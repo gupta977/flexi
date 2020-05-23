ba894c9d97476e96f7eef49dacee510b1241320b
ODude Navneet Gupta <navneet@odude.com>
2020-05-23 16:38:30 +0545

    1.0.31

--- public/partials/layout/detail/attach.php
+++ public/partials/layout/detail/attach.php
@@ -1,8 +1,8 @@
 <?php
-//$detail_layout = flexi_get_option('detail_layout', 'flexi_detail_settings', 'basic');
+
 $detail_layout = get_post_meta($post->ID, 'flexi_layout', 'basic');
 if ('default' == $detail_layout) {
- $detail_layout = 'basic';
+ $detail_layout = flexi_get_option('detail_layout', 'flexi_detail_settings', 'basic');
 }
 $file = FLEXI_PLUGIN_DIR . 'public/partials/layout/detail/' . $detail_layout . '/single.php';
 if (file_exists($file)) {
