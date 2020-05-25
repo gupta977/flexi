f1783d4c436cab7822beec2817c873b6c796d168
ODude Navneet Gupta <navneet@odude.com>
2020-05-25 22:43:32 +0545

    1.0.32

--- public/partials/layout/gallery/basic/loop.php
+++ public/partials/layout/gallery/basic/loop.php
@@ -2,13 +2,13 @@
 $data = flexi_image_data('thumbnail', $post, $popup);
 
 echo '<div class="flexi_responsive flexi_gallery_child" id="flexi_' . get_the_ID() . '"  data-tags="' . $tags . '">';
-echo '<div class="flexi_gallery_grid flexi_effect" id="' . $hover_effect . '">';
+echo '<div class="flexi_gallery_grid">';
 ?>
 
 <div class="flexi-list-small">
 
         <?php
-echo '<div class="flexi-image-wrapper flexi-list-sub ' . $data['popup'] . '">';
+echo '<div class="flexi-image-wrapper flexi-list-sub ' . $data['popup'] . ' flexi_effect" id="' . $hover_effect . '">';
 echo '<a class="" href="' . $data['url'] . '" data-caption="' . $data['title'] . '" border="0">';
 echo '<img src="' . esc_url(flexi_image_src('medium', $post)) . '">';
 ?>
