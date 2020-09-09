dc4205f45b389153ed3759f30cd5fca1188bc8fe
Wyrus © <navneet@odude.com>
2020-09-09 13:44:58 +0545

    2.3

---
+++ public/partials/layout/gallery/table/header.php
@@ -0,0 +1,19 @@
+<div id="flexi_gallery" class="flexi_basic_width_<?php echo $width; ?>">
+<div id="flexi_main_loop">
+
+<div class="flexi_table_container">
+  <table class="flexi-responsive-table">
+
+  <thead>
+      <tr>
+        <th scope="col">Film Title</th>
+        <th scope="col">Released</th>
+        <th scope="col">Studio</th>
+        <th scope="col">Worldwide Gross</th>
+        <th scope="col">Domestic Gross</th>
+        <th scope="col">Foreign Gross</th>
+        <th scope="col">Budget</th>
+      </tr>
+    </thead>
+
+    <tbody>
---
+++ public/partials/layout/gallery/table/loop.php
@@ -0,0 +1,10 @@
+<?php
+$data = flexi_image_data('thumbnail', $post, $popup);
+echo '<div class="flexi_responsive flexi_gallery_child" id="flexi_' . get_the_ID() . '"  data-tags="' . $tags . '">';
+echo '<div class="flexi_gallery_grid">';
+
+echo "<div a='i'>hiii</div>";
+?>
+     
+      </div>
+</div>      
