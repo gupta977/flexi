bde7667ebcd562e54d965d697520a67566a59504
Wyrus © <navneet@odude.com>
2020-09-11 09:34:34 +0545

    2.3

--- public/partials/layout/gallery/table/header.php
+++ public/partials/layout/gallery/table/header.php
@@ -1,6 +1,6 @@
 <ul class="flexi-responsive-table">
 <li class="flexi-table-header">
-      <div class="flexi-col flexi-col-1">Job Id</div>
+      <div class="flexi-col flexi-col-1"></div>
       <div class="flexi-col flexi-col-2">Customer Name</div>
       <div class="flexi-col flexi-col-3">Amount Due</div>
       <div class="flexi-col flexi-col-4">Payment Status</div>
--- public/partials/layout/gallery/table/loop.php
+++ public/partials/layout/gallery/table/loop.php
@@ -2,8 +2,26 @@
 $data = flexi_image_data('thumbnail', $post, $popup);
 ?>
     <li class="flexi-table-row">
-      <div class="flexi-col flexi-col-1" data-label="Job Id">42235</div>
+      <div class="flexi-col flexi-col-1" data-label="ddd">
+        
+      <?php
+echo '<div class="' . $data['popup'] . '">';
+echo '<a ' . $data['extra'] . ' href="' . $data['url'] . '" data-caption="' . $data['title'] . '" data-src="' . $data['src'] . '" border="0">';
+echo '<img src="' . esc_url(flexi_image_src('thumbnail', $post)) . '">';
+echo '</a>';
+echo '</div>';
+?>
+
+
+      </div>
       <div class="flexi-col flexi-col-2" data-label="Customer Name"> <?php echo $data['title']; ?></div>
       <div class="flexi-col flexi-col-3" data-label="Amount">$350</div>
-      <div class="flexi-col flexi-col-4" data-label="Payment Status">Pending</div>
+      <div class="flexi-col flexi-col-4" data-label="Payment Status">
+        
+<?php if (flexi_evalue_toggle('custom', $evalue) == '') {
+ echo '<span>' . flexi_custom_field_loop($post, 'gallery', 2) . '</span>';
+}
+?>
+
+      </div>
     </li>
