a9faf8179d009511cc95c86ddf41911e6f007df3
Wyrus © <navneet@odude.com>
2020-09-11 17:51:15 +0545

    2.3

--- public/partials/layout/gallery/table/header.php
+++ public/partials/layout/gallery/table/header.php
@@ -1,8 +1,29 @@
 <ul class="flexi-responsive-table">
 <li class="flexi-table-header">
-      <div class="flexi-col flexi-col-1"></div>
+    
       <div class="flexi-col flexi-col-2"><?php echo __("Title","flexi"); ?></div>
-      <div class="flexi-col flexi-col-3">Amount Due</div>
-      <div class="flexi-col flexi-col-4">Payment Status</div>
+   
+     
+      <?php
+ $count = 10;
+ $c = 1;
+ if (flexi_evalue_toggle('custom', $evalue) == '') {
+     for ($x = 1; $x <= 10; $x++) {
+         $label   = flexi_get_option('flexi_field_' . $x . '_label', 'flexi_custom_fields', '');
+         $display = flexi_get_option('flexi_field_' . $x . '_display', 'flexi_custom_fields', '');
+         if (is_array($display)) {
+             if (in_array('gallery', $display)) {
+                 echo '<div class="flexi-col flexi-col-4">'. $label .'</div>';
+
+                 if ($count == $c) {
+                     break;
+                 }
+                 $c++;
+             }
+         }
+     }
+ }
+?>
+  <div class="flexi-col flexi-col-1"></div>
     </li>
 	<div id="flexi_main_loop">
--- public/partials/layout/gallery/table/loop.php
+++ public/partials/layout/gallery/table/loop.php
@@ -2,20 +2,9 @@
 $data = flexi_image_data('thumbnail', $post, $popup);
 ?>
     <li class="flexi-table-row">
-      <div class="flexi-col flexi-col-1">
-        
-      <?php
-echo '<div class="' . $data['popup'] . '">';
-echo '<a ' . $data['extra'] . ' href="' . $data['url'] . '" data-caption="' . $data['title'] . '" data-src="' . $data['src'] . '" border="0">';
-echo '<img src="' . esc_url(flexi_image_src('thumbnail', $post)) . '">';
-echo '</a>';
-echo '</div>';
-?>
 
-
-      </div>
       <div class="flexi-col flexi-col-2" data-label="<?php echo __("Title","flexi"); ?>">
-       <?php echo $data['title']; ?>
+      <div class="flexi_title_bold"> <?php echo $data['title']; ?></div>
        <?php
 if (flexi_evalue_toggle('category', $evalue) == '') {
       echo '<span><div class="flexi_text_group">' . flexi_list_tags($post, "", "flexi_text_small", "dashicons dashicons-category", "flexi_category") . ' </div></span>';
@@ -26,13 +15,41 @@ if (flexi_evalue_toggle('category', $evalue) == '') {
      }
        ?>
        </div>
-      <div class="flexi-col flexi-col-3" data-label="Amount">$350</div>
-      <div class="flexi-col flexi-col-4" data-label="Payment Status">
-        
-<?php if (flexi_evalue_toggle('custom', $evalue) == '') {
- echo '<span>' . flexi_custom_field_loop($post, 'gallery', 2) . '</span>';
-}
-?>
+     
+      <?php
+ $count = 10;
+ $c = 1;
+ if (flexi_evalue_toggle('custom', $evalue) == '') {
+     for ($x = 1; $x <= 10; $x++) {
+         $label   = flexi_get_option('flexi_field_' . $x . '_label', 'flexi_custom_fields', '');
+         $display = flexi_get_option('flexi_field_' . $x . '_display', 'flexi_custom_fields', '');
+         $value   = get_post_meta($post->ID, 'flexi_field_' . $x, '');
+         if (!$value) {
+            $value[0] = '';
+           }
+         if (is_array($display)) {
+             if (in_array('gallery', $display)) {
+                 echo ' <div class="flexi-col flexi-col-4" data-label="'.$label.'">'.$value[0].'</div>';
 
-      </div>
+                 if ($count == $c) {
+                     break;
+                 }
+                 $c++;
+             }
+         }
+     }
+ }
+?>
+      <div class="flexi-col flexi-col-1">
+        
+        <?php
+  echo '<div class="' . $data['popup'] . '">';
+  echo '<a ' . $data['extra'] . ' href="' . $data['url'] . '" data-caption="' . $data['title'] . '" data-src="' . $data['src'] . '" border="0">';
+  echo '<img src="' . esc_url(flexi_image_src('thumbnail', $post)) . '">';
+  echo '</a>';
+  echo '</div>';
+  ?>
+  
+  
+        </div>
     </li>
