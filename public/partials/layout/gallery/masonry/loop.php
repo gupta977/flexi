ae4d56237d9beef84273a4ffd9e0f14d21323626
John Doe <navneet@odude.com>
2020-12-27 13:43:34 +0545

    3.1

--- public/partials/layout/gallery/masonry/loop.php
+++ public/partials/layout/gallery/masonry/loop.php
@@ -1,19 +1,18 @@
 <?php
 $data = flexi_image_data('thumbnail', get_the_ID(), $popup);
 ?>
-<div class="flexi_gallery_child flexi_padding" id="flexi_<?php echo get_the_ID(); ?>" style="position: relative;"
-      data-tags="<?php echo $tags; ?>">
+<div class="flexi_gallery_child flexi_padding" id="flexi_<?php echo get_the_ID(); ?>" style="position: relative;" data-tags="<?php echo $tags; ?>">
       <div class="flexi_masonry-item">
             <div class="flexi_effect <?php echo $data['popup']; ?>" id="<?php echo $hover_effect; ?>">
 
-                  <a
-                        <?php echo $data['extra'] . ' href="' . $data['url'] . '" data-src="' . $data['src'] . '" data-caption="' . $data['title'] . '" border="0"'; ?>>
+                  <a <?php echo $data['extra'] . ' href="' . $data['url'] . '" data-src="' . $data['src'] . '" data-caption="' . $data['title'] . '" border="0"'; ?>>
                         <img class="flexi-fit_cover" src="<?php echo esc_url(flexi_image_src('medium', $post)); ?>">
 
                         <div id="flexi_info" class="<?php echo $hover_caption; ?>">
                               <div class="flexi_title"><?php echo $data['title']; ?></div>
                               <div class="flexi_p"><?php echo flexi_excerpt(); ?></div>
                         </div>
+                        <div class="flexi_figcaption"><?php echo $data['title']; ?></div>
                   </a>
 
             </div>
