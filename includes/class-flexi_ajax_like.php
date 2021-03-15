bd3d5eeda0c4da0731d9534d506383b2a8aacede
John Doe <navneet@odude.com>
2021-03-15 13:51:16 +0545

    3.6

--- includes/class-flexi_ajax_like.php
+++ includes/class-flexi_ajax_like.php
@@ -7,7 +7,7 @@ class flexi_like
         add_action('wp_enqueue_scripts', array($this, "enqueue_script"));
         add_action("wp_ajax_flexi_ajax_like", array($this, "flexi_ajax_like"));
         add_action("wp_ajax_nopriv_flexi_ajax_like", array($this, "flexi_ajax_like"));
-        //add_action('flexi_loop_portfolio', array($this, 'display_like'));
+        add_action('flexi_loop_portfolio', array($this, 'display_like'));
     }
 
     //include js file
@@ -21,29 +21,89 @@ class flexi_like
     public function flexi_ajax_like()
     {
 
-        if (!wp_verify_nonce($_REQUEST['nonce'], "flexi_ajax_delete")) {
+        if (!wp_verify_nonce($_REQUEST['nonce'], "flexi_ajax_like")) {
             exit("No naughty business please");
         }
         $post_id = $_REQUEST["post_id"];
+        $key = $_REQUEST["key_type"];
+        $count = 0;
+        if ($key == 'like') {
+            $this->increase_like($post_id, 'flexi_like_count');
+            $count = $this->get_like_count($post_id, 'flexi_like_count');
+        }
+
+        if ($key == 'unlike') {
+            $this->increase_like($post_id, 'flexi_unlike_count');
+            $count = $this->get_like_count($post_id, 'flexi_unlike_count');
+        }
+
+        if (true) {
+            $data = true;
+        } else {
+            $data = false;
+        }
+
+        if (false === $data) {
+            $result['type']       = "error";
+            $result['data_count'] = $count;
+        } else {
+            $result['type']       = "success";
+            $result['data_count'] = $count;
+        }
+
+        if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            $result = json_encode($result);
+            echo $result;
+        } else {
+            header("Location: " . $_SERVER["HTTP_REFERER"]);
+        }
+
+        die();
     }
 
     public function display_like()
     {
+        $nonce   = wp_create_nonce("flexi_ajax_like");
+        $id = get_the_ID();
 ?>
         <span class="fl-icon-text fl-tag">
-            <span id="abc" class="fl-icon">
+            <span id="flexi_like" data-key_type="like" data-nonce="<?php echo $nonce; ?>" data-post_id="<?php echo $id; ?>" class="fl-icon">
                 <i class="fas fa-thumbs-up"></i>
             </span>
-            <span><?php echo get_the_ID(); ?></span>
+            <span id="flexi_like_count_<?php echo $id; ?>"><?php echo $this->get_like_count($id, 'flexi_like_count'); ?></span>
         </span>
         <span class="fl-icon-text fl-tag">
-            <span id="abc" class="fl-icon">
+            <span id="flexi_like" data-key_type="unlike" data-nonce="<?php echo $nonce; ?>" data-post_id="<?php echo $id; ?>" class="fl-icon">
                 <i class="fas fa-thumbs-down"></i>
             </span>
-            <span><?php echo get_the_ID(); ?></span>
+            <span id="flexi_unlike_count_<?php echo $id; ?>"><?php echo $this->get_like_count($id, 'flexi_unlike_count'); ?></span>
         </span>
 
 <?php
+    }
+    //Total number of like & unlike
+    public function get_like_count($id, $key)
+    {
+        $count = get_post_meta($id, $key, true);
+        return $count;
+    }
+
+    //Increase like
+    public function increase_like($post_id, $key)
+    {
+
+        $count = (int) get_post_meta($post_id, $key, true);
+        $count++;
+        update_post_meta($post_id, $key, $count);
+    }
+
+    //Decrease likes
+    public function decrease_like($post_id, $key)
+    {
+
+        $count = (int) get_post_meta($post_id, $key, true);
+        $count--;
+        update_post_meta($post_id, $key, $count);
     }
 }
 
--- public/js/flexi_ajax_like.js
+++ public/js/flexi_ajax_like.js
@@ -1,43 +1,39 @@
 jQuery(document).ready(function () {
-  jQuery(document).on("click", "#flexi_ajax_like", function (e) {
+  jQuery(document).on("click", "#flexi_like", function (e) {
     e.preventDefault();
     post_id = jQuery(this).attr("data-post_id");
     media_id = jQuery(this).attr("data-media_id");
+    key_type = jQuery(this).attr("data-key_type");
     //alert(post_id+'--'+media_id);
     nonce = jQuery(this).attr("data-nonce");
-    var x = confirm(myAjax.delete_string);
-    if (x) {
-      jQuery.ajax({
+       jQuery.ajax({
         type: "post",
         dataType: "json",
         url: myAjax.ajaxurl,
-        data: { action: "flexi_ajax_like", post_id: post_id, nonce: nonce, media_id:media_id },
+        data: { action: "flexi_ajax_like", post_id: post_id, nonce: nonce, media_id:media_id,key_type:key_type },
         success: function (response) {
           if (response.type == "success") {
-            if(media_id)
+            if(post_id)
             {
               //jQuery("#flexi_media_" + media_id).slideUp("slow");
-              alert("aa");
+             alert(response.data_count);
+            // jQuery("#flexi_like_count_"+post_id).slideUp("slow");
+             jQuery("#flexi_like_count_"+post_id).empty();
+            jQuery("#flexi_like_count_"+post_id).append(response.data_count).fadeIn("slow");
             }
             else
             {
              // jQuery("#flexi_content_" + post_id).slideUp("slow");
              // jQuery("#flexi_" + post_id).slideUp();
-             alert("ggg");
+             alert("alrfeady voted");
             }
             
                         
           } else {
-            alert("Deleted: " + post_id);
+            alert("some error: " + post_id);
           }
         },
       });
-    }
-  });
-  jQuery("#abc").click(function (e) {
-    alert("hi");
-  });
-  jQuery(".xyz").click(function (e) {
-    // alert("bye");
+    
   });
 });
