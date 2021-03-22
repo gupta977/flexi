ab4937e9d6be6b3b530db8c87a6d2577eb906c39
John Doe <navneet@odude.com>
2021-03-22 17:57:30 +0545

    3.6

--- public/partials/layout/gallery/portfolio/loop.php
+++ public/partials/layout/gallery/portfolio/loop.php
@@ -25,7 +25,7 @@ if ($column == "1") {
     </div>
     <div class="fl-card-content">
       <div class="fl-title fl-is-4 fl-mb-1" style="<?php echo flexi_evalue_toggle('title', $evalue); ?>"><?php echo $data['title']; ?></div>
-      <div class="fl-content fl-mb-1" style="<?php echo flexi_evalue_toggle('excerpt', $evalue); ?>">
+      <div class="fl-content fl-mb-1 fl-is-size-6" style="<?php echo flexi_evalue_toggle('excerpt', $evalue); ?>">
         <?php echo flexi_excerpt(20); ?>
       </div>
 
